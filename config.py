c6a6294cd85f9316e39542dc828fbecd554148c7
sanmart02 <113845440+sanmart02@users.noreply.github.com>
2023-04-01 19:07:10 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@
 from os import environ
 
 class Config(object):
-    TG_BOT_TOKEN = environ.get("BOT_TOKEN", "") # Make a bot from https://t.me/BotFather and enter the token here
+    TG_BOT_TOKEN = "6144753157:AAHfsBvjQWdaOAI1My-6JVeB5zbdD9hWTXM" # Make a bot from https://t.me/BotFather and enter the token here
     
-    APP_ID = int(environ.get("API_ID", 12345)) # Get this value from https://my.telegram.org/apps
+    APP_ID = 27885485 # Get this value from https://my.telegram.org/apps
     
-    API_HASH = environ.get("API_HASH", "") # Get this value from https://my.telegram.org/apps
+    API_HASH = "7dd9974c713787410beae4a295cc1e2d" # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(environ.get("OWNER_ID", None)) # Your(owner's) telegram id
+    OWNER_ID = 5663539940 # Your(owner's) telegram id
     
-    MONGO_STR = environ.get("MONGO_STR", "") # Get from MongoDB Atlas
+    MONGO_STR = "mongodb+srv://sanmart02:sanmart02@uploader.qn5vziv.mongodb.net/?retryWrites=true&w=majority" # Get from MongoDB Atlas
 
     DOWNLOAD_LOCATION = "app//DOWNLOADS//" # The download location for users. (Don't change anything in this field!)
 
